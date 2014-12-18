8e5fbec22f78977e32d0c76f328980c2b5599ad4
eddietree <eddie@illogictree.com>
2014-12-17 23:09:05 -0800

    added core class

---
+++ Core.py
@@ -0,0 +1,27 @@
+import pygame
+import math
+
+black = (0,0,0)
+white = (255,255,255)
+y = 0
+
+class Core:
+    """A simple example class"""
+    _time = 0.0
+
+    def __init__(self):
+
+    	#create the screen
+		global g_window
+		g_window = pygame.display.set_mode((640, 480)) 
+
+    def update(self):
+    	self._time += 1.0/60.0
+
+    def draw(self):
+
+    	g_window.fill((black))
+    	global y
+    	y = 50 + math.sin(self._time * 0.1) * 100
+    	pygame.draw.line(g_window, (255, 255, 255), (200, y), (230, 50+y))
+    	pygame.display.flip() 
--- main.py
+++ main.py
@@ -1,47 +1,25 @@
 import sys
 import pygame
 import math
+from Core import *
 
-pygame.init() 
-
-window = 0
-black = (0,0,0)
-white = (255,255,255)
-y = 0
-time = 0.0
+g_window = 0
+g_core = 0
 
 def init():
-	#create the screen
-	global window
-	window = pygame.display.set_mode((640, 480)) 
 
-def draw():
-
-	window.fill((black))
-
-	global y
-
-	y = 50 + math.sin(time) * 100
-
-	#draw a line - see http://www.pygame.org/docs/ref/draw.html for more 
-	pygame.draw.line(window, (255, 255, 255), (200, y), (230, 50+y))
-
-	for xx in range(0, 12):	
-  		for yy in range(0, 16):
-			pygame.draw.ellipse(window, white, [math.cos(time+yy)*20.0 + xx * 60, 10 + 30 *yy , 50, 20] )
-
-	#draw it to the screen
-	pygame.display.flip() 
-
-
+	pygame.init() 
+	
+	global g_core
+	g_core = Core()
+	
 init()
 
-
-#input handling (somewhat boilerplate code):
 while True: 
-   time += 1.0/60.0
 
-   draw()
+   g_core.update()
+   g_core.draw()
+
    for event in pygame.event.get(): 
       if event.type == pygame.QUIT: 
           sys.exit(0) 
