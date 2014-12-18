6e38fe217f2c5c1e856debb562865b4c715e86d1
eddietree <eddie@illogictree.com>
2014-12-18 00:46:48 -0800

    line expand visual

--- SceneIntro.py
+++ SceneIntro.py
@@ -14,17 +14,22 @@ class ItemCircle(Item.Item):
         screen_height = Globals.screen_size[1]
         screen_center_x = screen_width * 0.5
         screen_center_y = screen_height * 0.5
-
         min_dimen = min(screen_width,screen_height)
 
-        rect_width_delta = min_dimen * 0.1
+        line_thickness = 3
+        num_rings = 6
+        rect_width_delta = min_dimen * 0.09
+        time = Globals.time * 2.0
 
-        for i in range(1, 5):
-            rect_width = rect_width_delta * i
-            pygame.draw.rect(Globals.window, Globals.WHITE, [screen_center_x - rect_width*0.5, screen_center_y - rect_width*0.5, rect_width, rect_width], 1)
+        rect_width_max = num_rings * rect_width_delta
 
-        y = 50 + math.sin(Globals.time * 0.1) * 100
-        pygame.draw.line(Globals.window, (255, 255, 255), (200, y), (230, 50+y))
+        for i in range(0, num_rings):
+            rect_width = (rect_width_delta * i + time) % rect_width_max
+            
+            alpha = math.sin( math.pi * rect_width / rect_width_max )
+            color = (255*alpha, 255*alpha, 255*alpha)
+
+            pygame.draw.rect(Globals.window, color, [screen_center_x - rect_width*0.5, screen_center_y - rect_width*0.5, rect_width, rect_width], line_thickness)
 
 class SceneIntro(Scene.Scene):
     """A simple scene class"""
